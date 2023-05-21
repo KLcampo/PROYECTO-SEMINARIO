4a55afaaf51863006c3ca9440f2e2c87a6148951
KLcampo <karencampo299@gmail.com>
2023-05-20 19:35:03 -0500

    Creaccion startapp y BD

---
+++ Destinos/Otras/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class OtrasConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Otras'
---
+++ Destinos/Resturantes/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ResturantesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Resturantes'
