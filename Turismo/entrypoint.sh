eda4ed79c3cd79bde213d8ae6b50e913330f7e2f
KLcampo <karencampo299@gmail.com>
2023-05-22 12:40:40 -0500

    docker

---
+++ Turismo/entrypoint.sh
@@ -0,0 +1,14 @@
+
+if [ "$DATABASE" = "POSTGRES" ]
+then
+   
+   echo "Waiting for postgres..."
+
+   while | nc -z $SQL_HOST $SQL_PORT; do
+    sleep 0.1
+   done
+
+   echo "PostgresSQL started"
+fi
+
+exec "$@"
