a9554151f71b9df9a42b8f14f7d0142b46a4a47f
KLcampo <karencampo299@gmail.com>
2023-05-22 14:01:58 -0500

    docker compose proyecto

--- Turismo/restaurantes/urls.py
+++ Turismo/restaurantes/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import Restaurantes_APIView, Restaurantes_APIView_List
+from .views import Restaurantes_APIView, Restaurantes_APIView_List, Restaurantes_APIView_Detail, Ubicaciones_APIView, Ubicaciones_APIView_List, Ubicaciones_APIView_Detail
 
 urlpatterns = [
     path('v1/rest/add', Restaurantes_APIView.as_view()),
-    path('v1/rest', Restaurantes_APIView_List.as_view())
+    path('v1/rest', Restaurantes_APIView_List.as_view()),
+    path('v1/rest/<int:pk>', Restaurantes_APIView_Detail.as_view()),
+     path('v1/ubi/add', Ubicaciones_APIView.as_view()),
+    path('v1/ubi', Ubicaciones_APIView_List.as_view()),
+    path('v1/ubi/<int:pk>', Ubicaciones_APIView_Detail.as_view())
+
+
 ]
--- Turismo/restaurantes/views.py
+++ Turismo/restaurantes/views.py
@@ -25,6 +25,72 @@ class Restaurantes_APIView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+class Restaurantes_APIView_Detail(APIView):
+
+    def get_object(self, pk):
+        try: 
+            return Restaurantes.objects.get(pk=pk)
+        except Restaurantes.DoesNotExist:
+            raise Http404
+    def get(self, request, pk, format=None):
+        restaurante = self.get_object(pk)
+        serializer = RestaurantesSerializers(restaurante)
+        return Response(serializer.data)
+    
+    def put(self, request, pk, format=None):
+        restaurante = self.get_object(pk)
+        serializer = RestaurantesSerializers(restaurante, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk, format=None):
+        restaurante = self.get_object(pk)
+        restaurante.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
+class Ubicaciones_APIView_List(APIView):
+    def get(self, request, format=None, *args, **kwargs):
+        ubicacion = Ubicaciones.objects.all()
+        serializers = UbicacionesSerializers(ubicacion, many=True)
+        return Response(serializers.data)
+
+class Ubicaciones_APIView(APIView):
+        
+    def post(self, request, format=None):
+        print(request.data)
+        serializer = UbicacionesSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class Ubicaciones_APIView_Detail(APIView):
+
+    def get_object(self, pk):
+        try: 
+            return Ubicaciones.objects.get(pk=pk)
+        except Ubicaciones.DoesNotExist:
+            raise Http404
+    def get(self, request, pk, format=None):
+        ubicacion = self.get_object(pk)
+        serializer = UbicacionesSerializers(ubicacion)
+        return Response(serializer.data)
+    
+    def put(self, request, pk, format=None):
+        ubicacion = self.get_object(pk)
+        serializer = UbicacionesSerializers(ubicacion, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk, format=None):
+        ubicacion = self.get_object(pk)
+        ubicacion.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
     
 
 
